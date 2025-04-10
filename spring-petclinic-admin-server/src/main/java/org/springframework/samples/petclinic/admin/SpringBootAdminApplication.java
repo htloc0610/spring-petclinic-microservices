7edf242e9e57fff38912b8bf9123b7acbff53924
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2025-04-10 23:17:10 +0700

    Check

--- spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
+++ spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
@@ -25,6 +25,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class SpringBootAdminApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAdminApplication.class, args);
     }
